cdc5231517081856f5aa6dc4527f54e5de169e39
Myron <shikhovmyron@gmail.com>
2022-07-09 17:05:54 +0200

    add base test for solution

--- src/test/java/easy/ValidParenthesesTest.java
+++ src/test/java/easy/ValidParenthesesTest.java
@@ -1,7 +1,21 @@
 package easy;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ValidParenthesesTest {
+    ValidParentheses validParentheses = new ValidParentheses();
 
+    @ParameterizedTest
+    @CsvSource({
+            "(),true",
+            "(){}[],true",
+            "(},false",
+    })
+    void isValidTest(String s, boolean expected) {
+        boolean actual = validParentheses.isValid(s);
+        assertEquals(expected, actual);
+    }
 }
