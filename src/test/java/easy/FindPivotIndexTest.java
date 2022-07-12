fbf6fec5d8f8c8e52193e44576b9bbf1f5d4de32
Myron <shikhovmyron@gmail.com>
2022-07-12 11:39:53 +0200

    Add new solution "724. Find Pivot Index"

---
+++ src/test/java/easy/FindPivotIndexTest.java
@@ -0,0 +1,27 @@
+package easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FindPivotIndexTest {
+    private final FindPivotIndex fourSum = new FindPivotIndex();
+
+    static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 7, 3, 6, 5, 6}, 3),
+                Arguments.of(new int[]{1, 2, 3}, -1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void pivotIndexTest(int[] nums, int expected) {
+        int actual = fourSum.pivotIndex(nums);
+        assertEquals(expected, actual);
+    }
+}
