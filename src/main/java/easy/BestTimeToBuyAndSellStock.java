fb2c8b52751d9d55680c62439ea8ffccba7560b6
Myron <shikhovmyron@gmail.com>
2022-07-16 13:11:20 +0200

    add problem-solving "121. Best Time to Buy and Sell Stock"

--- src/main/java/easy/BestTimeToBuyAndSellStock.java
+++ src/main/java/easy/BestTimeToBuyAndSellStock.java
@@ -2,6 +2,13 @@ package easy;
 
 public class BestTimeToBuyAndSellStock {
     public int maxProfit(int[] prices) {
+        int lsf = prices[0];
+        int op = 0;
 
+        for (int i = 1; i < prices.length; i++) {
+            if (lsf > prices[i]) lsf = prices[i];
+            op = Math.max(op, prices[i] - lsf);
+        }
+        return op;
     }
 }
