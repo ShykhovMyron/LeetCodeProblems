e1cbe6dba64b21a0c763fd705d3cccbb9471d290
Myron <shikhovmyron@gmail.com>
2022-07-23 11:57:39 +0200

    add problem-solving "98. Validate Binary Search Tree"

---
+++ src/main/java/easy/LastStoneWeight.java
@@ -0,0 +1,16 @@
+package easy;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int stone : stones) {
+            queue.add(stone);
+        }
+
+
+        return queue.isEmpty() ? 0 : queue.remove();
+    }
+}
