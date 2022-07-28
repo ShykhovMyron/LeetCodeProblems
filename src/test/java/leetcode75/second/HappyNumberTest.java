67d2a0121a2175d1e6789f625ddda56578896646
Myron <shikhovmyron@gmail.com>
2022-07-28 19:59:19 +0200

    Add problem-solving "202. Happy Number"

--- src/test/java/leetcode75/second/HappyNumberTest.java
+++ src/test/java/leetcode75/second/HappyNumberTest.java
@@ -1,15 +1,7 @@
 package leetcode75.second;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import java.util.List;
-
-import static org.mockito.Mockito.*;
 
 class HappyNumberTest {
 
