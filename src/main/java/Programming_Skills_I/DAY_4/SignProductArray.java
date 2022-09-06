bb7d8f43368de12ce815389ac44ff3286f48d4f8
Myron <shikhovmyron@gmail.com>
2022-09-06 15:17:16 +0200

    add new problem-solving (1822. Sign of the Product of an Array)

---
+++ src/main/java/Programming_Skills_I/DAY_4/SignProductArray.java
@@ -0,0 +1,12 @@
+package Programming_Skills_I.DAY_4;
+
+public class SignProductArray {
+    public int arraySign(int[] nums) {
+        int prod = 1;
+        for (int num : nums) {
+            if (num < 0) prod = -prod;
+            else if (num == 0) return 0;
+        }
+        return prod;
+    }
+}
